cbbdafd2829e1476d3ebe501dc12ec084b57ccc1
Carlos Hernández <carlos_t815@outlook.com>
2020-07-20 21:57:56 -0400

    Updates

---
+++ precache-manifest.a81aa910b565d8f105efc8695d85cc31.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5e7b70e403a5ebe1186610ef44f57d",
+    "url": "/CC-clone/index.html"
+  },
+  {
+    "revision": "d41f6735a05210b28cc5",
+    "url": "/CC-clone/static/css/main.2e575870.chunk.css"
+  },
+  {
+    "revision": "8b33768a5cd37be106ce",
+    "url": "/CC-clone/static/js/2.b13752b3.chunk.js"
+  },
+  {
+    "revision": "ff78069088dbc9e665a2f51e9a7e7f98",
+    "url": "/CC-clone/static/js/2.b13752b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d41f6735a05210b28cc5",
+    "url": "/CC-clone/static/js/main.5b35145f.chunk.js"
+  },
+  {
+    "revision": "14795e7a12061d70a1b1",
+    "url": "/CC-clone/static/js/runtime-main.30253081.js"
+  },
+  {
+    "revision": "bf8473a448870b6bce565f3dd1563572",
+    "url": "/CC-clone/static/media/IMG-8012-scaled.bf8473a4.jpg"
+  },
+  {
+    "revision": "8616ce44c921e34dae9733a7a25e8ec2",
+    "url": "/CC-clone/static/media/Image-118.8616ce44.png"
+  },
+  {
+    "revision": "968b49fa29ac77e8aa3bf0be723307bf",
+    "url": "/CC-clone/static/media/arepa.min.968b49fa.svg"
+  },
+  {
+    "revision": "8c4090a05c1f700189faa5c294a7a42a",
+    "url": "/CC-clone/static/media/logo.min.8c4090a0.svg"
+  },
+  {
+    "revision": "e354045aa6e2a410072895cb7f27a103",
+    "url": "/CC-clone/static/media/rock_contra_la_dictadura_2_3-990x524.e354045a.jpg"
+  }
+]);
